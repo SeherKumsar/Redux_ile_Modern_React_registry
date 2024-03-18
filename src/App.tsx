a5339538ccf7766c0f17d8998912a9f2debe6b07
SeherKumsar <seherkumsar99@outlook.com>
2024-03-18 23:33:15 +0300

    fixed DetailsPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Root from "./pages/Root";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/search/SearchPage";
 import { searchLoader } from "./pages/search/searchLoader";
-import DetailsPage from "./pages/DetailsPage";
+import DetailsPage from "./pages/details/DetailsPage";
 import { detailsLoader } from "./pages/details/detailsLoader";
 
 const router = createBrowserRouter([
